11017ee3c2b552a0a10c346eeb3fa3a439bca137
kenney <kenneycode@qq.com>
2019-03-16 12:16:53 +0800

    提交工程(commit the project)

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # OpenGLES2.0SamplesForAndroid
+
+
+
+这是一个Android上的OpenGL ES 2.0 学习工程，我会不断地补充sample，并在关键代码处附上中英文注释。
+
+This is a OpenGL 2.0 learning project for Android, and I will continue to code and commit samples and write chinese and english commets for some import code.
+
+
+
+
+目前工程中有1个例子:
+
+Now, this project contains 1 sample.
+
+- Hello World
+
+  一个简单的Hello World例子，渲染一个三角形
+
+  A simple Hello World, render a triangle.
+
---
+++ app/src/main/java/com/kenneycode/SampleActivity.kt
@@ -0,0 +1,43 @@
+package com.kenneycode
+
+import android.opengl.GLSurfaceView
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.kenneycode.samples.SampleHelloWorld
+
+/**
+ *
+ *      Coded by kenney
+ *
+ *      http://www.github.com/kenneycode
+ *
+ *      Sample展示类
+ *      This sample activity
+ *
+ **/
+
+class SimpleActivity : AppCompatActivity() {
+
+    private val samples = arrayOf(SampleHelloWorld())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sample)
+        val glSurfaceView = findViewById<GLSurfaceView>(R.id.glsurfaceview)
+        // 设置RGBA颜色缓冲、深度缓冲及stencil缓冲大小
+        // Set the size of RGBA、depth and stencil buffer
+        glSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 0, 0)
+        // 设置GL版本，这里设置为2.0
+        // Set GL version, here I set it to 2.0
+        glSurfaceView.setEGLContextClientVersion(2)
+        val sampleName = intent.getStringExtra(GlobalConstants.KEY_SAMPLE_NAME)
+        title = sampleName
+        val sampleIndex = intent.getIntExtra(GlobalConstants.KEY_SAMPLE_INDEX, -1)
+        if (sampleIndex >= 0) {
+            // 设置对应sample的渲染器
+            // Set the corresponding sample renderer
+            glSurfaceView.setRenderer(samples[sampleIndex])
+        }
+    }
+
+}
